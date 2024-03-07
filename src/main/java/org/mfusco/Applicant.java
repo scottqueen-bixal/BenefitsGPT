521673fa553b3cd1aec120e47de8395840138452
scottqueen-bixal <scott.queen@contractor.bixal.com>
2024-03-07 11:09:06 -0500

    formatting

--- src/main/java/org/mfusco/Applicant.java
+++ src/main/java/org/mfusco/Applicant.java
@@ -8,26 +8,42 @@ import java.time.LocalDate;
 // not-eligible
 // Today is Mar 5, 2024. The applicant Scott Queen was born in Sudan, but has dual citizenship including U.S. He turned 72 yesterday. His wife died while visiting her mother in Texas from COVID during the first year of the pandemic. He is now caring for a child who was born one year before his wife died. Scott paid for the funeral expenses and was reimbursement.
 
-public record Applicant(String applicantFirstName, String applicantLastName, LocalDate applicantBirthDate, String applicantRelationshipToDeceased, String applicantMaritalStatus, Boolean applicantCitizenStatus, Boolean applicantPaidFuneralExpenses, Boolean applicantReimbursedFuneralExpenses, LocalDate deceasedDateOfDeath, Boolean deceasedDiedInUs, Boolean deceasedDiedFromCovid, Boolean applicantCaringForChild, Boolean childDisabled, LocalDate childAge)  {
+public record Applicant(
+        String applicantFirstName,
+        String applicantLastName,
+        LocalDate applicantBirthDate,
+        String applicantRelationshipToDeceased,
+        String applicantMaritalStatus,
+        Boolean applicantCitizenStatus,
+        Boolean applicantPaidFuneralExpenses,
+        Boolean applicantReimbursedFuneralExpenses,
+        Boolean deceasedPaidSocialSecurityTaxes,
+        LocalDate deceasedDateOfDeath,
+        Boolean deceasedDiedInUs,
+        Boolean deceasedDiedFromCovid,
+        Boolean applicantCaringForChild,
+        Boolean childDisabled,
+        LocalDate childAge) {
 
     @Override
     public String toString() {
         return "Applicant {" +
-            " applicantFirstName = \"" + applicantFirstName + "\"" +
-            ", applicantLastName = \"" + applicantLastName + "\"" +
-            ", applicantBirthDate = " + applicantBirthDate +
-            ", applicantRelationshipToDeceased = " + applicantRelationshipToDeceased +
-            ", applicantMaritalStatus = " + applicantMaritalStatus +
-            ", applicantCitizenStatus = " + applicantCitizenStatus +
-            ", applicantPaidFuneralExpenses = " + applicantPaidFuneralExpenses +
-            ", applicantReimbursedFuneralExpenses = " + applicantReimbursedFuneralExpenses +
-            ", deceasedDiedInUs = " + deceasedDiedInUs +
-            ", deceasedDiedFromCovid = " + deceasedDiedFromCovid +
-            ", deceasedDateOfDeath = " + deceasedDateOfDeath +
-            ", applicantCaringForChild = " + applicantCaringForChild +
-            ", childDisabled = " + childDisabled +
-            ", childAge = " + childAge +
-            " }";
+                " applicantFirstName = \"" + applicantFirstName + "\"" +
+                ", applicantLastName = \"" + applicantLastName + "\"" +
+                ", applicantBirthDate = " + applicantBirthDate +
+                ", applicantRelationshipToDeceased = " + applicantRelationshipToDeceased +
+                ", applicantMaritalStatus = " + applicantMaritalStatus +
+                ", applicantCitizenStatus = " + applicantCitizenStatus +
+                ", applicantPaidFuneralExpenses = " + applicantPaidFuneralExpenses +
+                ", applicantReimbursedFuneralExpenses = " + applicantReimbursedFuneralExpenses +
+                ", deceasedDiedInUs = " + deceasedDiedInUs +
+                ", deceasedPaidSocialSecurityTaxes = " + deceasedPaidSocialSecurityTaxes +
+                ", deceasedDiedFromCovid = " + deceasedDiedFromCovid +
+                ", deceasedDateOfDeath = " + deceasedDateOfDeath +
+                ", applicantCaringForChild = " + applicantCaringForChild +
+                ", childDisabled = " + childDisabled +
+                ", childAge = " + childAge +
+                " }";
     }
 
     public String getFullName() {
