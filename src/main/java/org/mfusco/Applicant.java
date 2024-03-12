b7a938434e7dbe1dad531346954f3f452c73fb1d
scottqueen-bixal <scott.queen@contractor.bixal.com>
2024-03-12 10:14:21 -0400

    check relationship status based on applicable values

--- src/main/java/org/mfusco/Applicant.java
+++ src/main/java/org/mfusco/Applicant.java
@@ -1,6 +1,7 @@
 package org.mfusco;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 // likely-eligible
 // Today is Mar 5, 2024. The applicant Scott Queen was born in the US. He turned 42 yesterday. His wife died while visiting her mother in Texas from COVID during the second year of the pandemic. He is now caring for a special needs child who was born three years before his wife died. Scott paid for the funeral expenses, but never filed for reimbursement.
@@ -52,6 +53,15 @@ public record Applicant(
 
     public int getAge() {
         return applicantBirthDate.until(LocalDate.now()).getYears();
+    }
+
+    public boolean getApplicantRelationshipToDeceased(String[] applicantRelationships) {
+        for (String relationship : applicantRelationships) {
+            if (Arrays.asList(applicantRelationshipToDeceased).contains(relationship)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public int getDeath() {
